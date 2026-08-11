887ed53d300ccb0029bd20a056bb58fe874e9e64
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-08-11 22:35:31 +0100

    feat: add application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Routes, Route } from 'react-router'
+import BoardPage from './pages/BoardPage'
+import StatisticsPage from './pages/StatisticsPage'
+import SettingsPage from './pages/SettingsPage'
+import AssistantPage from './pages/AssistantPage'
 import './App.css'
 
 function App() {
-  return 
+  return (
+    <div className="app">
+      <main>
+        <Routes>
+          <Route index element={<BoardPage />} />
+          <Route path="statistics" element={<StatisticsPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+          <Route path="assistant" element={<AssistantPage />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
