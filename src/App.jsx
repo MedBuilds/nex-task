cd43bf62182c98bfc8016ff9f44ae87928dfb9ba
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-08-12 17:09:23 +0100

    style: add themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router'
 import BoardPage from './pages/BoardPage/BoardPage'
 import StatisticsPage from './pages/StatisticsPage/StatisticsPage'
@@ -6,8 +7,10 @@ import AssistantPage from './pages/AssistantPage/AssistantPage'
 import './App.css'
 
 function App() {
+  const [theme] = useState('light')
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <main>
         <Routes>
           <Route index element={<BoardPage />} />
