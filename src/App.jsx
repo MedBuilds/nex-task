01058d7d51a30395320558accd28f80c18b7759d
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-08-13 16:09:56 +0100

    feat: add default tasks data

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import BoardPage from './pages/BoardPage/BoardPage'
 import StatisticsPage from './pages/StatisticsPage/StatisticsPage'
 import SettingsPage from './pages/SettingsPage/SettingsPage'
 import AssistantPage from './pages/AssistantPage/AssistantPage'
+import Data from './data/data.json'
 import './App.css'
 
 function App() {
   const [ theme, setTheme ] = useState('light')
+  const [ tasks ] = useState(Data.tasks)
 
   return (
     <div className={`app ${theme}`}>
@@ -16,7 +18,7 @@ function App() {
       
       <main>
         <Routes>
-          <Route index element={<BoardPage />} />
+          <Route index element={<BoardPage tasks={tasks}/>} />
           <Route path="statistics" element={<StatisticsPage />} />
           <Route path="settings" element={<SettingsPage />} />
           <Route path="assistant" element={<AssistantPage />} />
