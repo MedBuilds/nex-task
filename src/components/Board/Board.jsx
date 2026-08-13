f7b195419510e3db3b93421b0f43e2e4b21baf89
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-08-13 16:08:05 +0100

    feat: add kanban board

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,14 @@
+import Column from "../Column/Column";
+import "./Board.css";
+
+function Board({ tasks }) {
+    return (
+        <div className="board">
+            <Column tasks={tasks} status="todo" />
+            <Column tasks={tasks} status="in-progress" />
+            <Column tasks={tasks} status="done" />
+        </div>
+    );
+}
+
+export default Board;
