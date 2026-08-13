ad8831e429c074d30cce3fd8b2ce14f0a3382679
MedBuilds <268667762+MedBuilds@users.noreply.github.com>
2026-08-13 16:11:20 +0100

    feat: render board on board page

--- src/pages/BoardPage/BoardPage.jsx
+++ src/pages/BoardPage/BoardPage.jsx
@@ -1,12 +1,14 @@
 import Header from "../../components/Header/Header";
+import Board from "../../components/Board/Board";
 import "./BoardPage.css";
 
-function BoardPage() {
+function BoardPage({tasks}) {
     return (
         <div className="board-page">
             <title>NexTask - Board</title>
 
             <Header title="Board" actions={true} />
+            <Board tasks={tasks}/>
         </div>
     );
 }
